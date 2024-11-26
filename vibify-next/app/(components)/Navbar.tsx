cc01ce184344f0ae4f8c30b9fb755f7b3836b4c5
Kevin Castro <kcapupp@princeton.edu>
2024-11-25 20:05:29 -0500

    Simplify navbar

--- vibify-next/app/(components)/Navbar.tsx
+++ vibify-next/app/(components)/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { ReactElement } from "react";
 import { usePathname } from "next/navigation";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import PersonIcon from "@mui/icons-material/Person";
@@ -12,70 +12,61 @@ const Navbar = () => {
 
   const currentPage = pathname?.split("/")[1];
 
+  interface NavbarElementProp {
+    href: string;
+    icon?:ReactElement;
+    text: string;
+    value: string;
+  }
+
+  const navbarElements: NavbarElementProp[] = [
+    {
+      href: "/dashboard",
+      icon:<DashboardIcon />,
+      text: "Dashboard",
+      value: "dashboard",
+    },{
+      href: "/recs",
+      icon:<LibraryMusicIcon/>,
+      text: "Recs",
+      value: "recs",
+    },{
+      href: "/tracks",
+      icon:<MusicNoteOutlinedIcon/>,
+      text: "Tracks",
+      value: "tracks",
+    },{
+      href: "/artists",
+      icon:<PersonIcon/>,
+      text: "Artists",
+      value: "artists",
+    },{
+      icon:<LibraryMusicIcon/>,
+      href: "/playlists",
+      text: "Playlists",
+      value: "playlists",
+    },
+  ];
   return (
-    <div className="md:h-screen bg-background w-screen fixed md:top-0 md:w-[100px]  bottom-0 md:block flex flex-col">
+    <div className="z-20 md:h-screen bg-background w-screen fixed md:top-0 md:w-[100px]  bottom-0 md:block flex flex-col">
       <div className="flex flex-col justify-center h-full">
         <ul className=" flex flex-row md:flex-col navbar-links text-center w-full">
-          <li
-            className={`flex box py-4 md:py-2 justify-center flex-1 text-sm flex-col items-center ${
-              currentPage === "dashboard"
-                ? " border-t-4 md:border-t-0 md:border-l-4 border-main bg-grayBg"
-                : ""
-            }`}
-          >
-            <Link href="/dashboard">
-              <DashboardIcon />
-              <div>Dashboard</div>
-            </Link>
-          </li>
-          <li
-            className={`flex box py-4 md:py-2 justify-center flex-1 text-sm flex-col items-center ${
-              currentPage === "recs"
-                ? " border-t-4 md:border-t-0 md:border-l-4 border-main bg-grayBg"
-                : ""
-            }`}
-          >
-            <Link href={"/recs"}>
-              <DashboardIcon />
-              <div>Recs</div>
-            </Link>
-          </li>
-          <li
-            className={`flex box py-4 md:py-2 justify-center flex-1 text-sm flex-col items-center   ${
-              currentPage === "artists"
-                ? " border-t-4 md:border-t-0 md:border-l-4 border-main"
-                : ""
-            }`}
-          >
-            <Link href={"/artists"}>
-              <PersonIcon />
-              <div>Top Artists</div>
-            </Link>
-          </li>
-          <li
-            className={`flex box py-4 md:py-2 justify-center flex-1 text-sm flex-col items-center   ${
-              currentPage === "songs"
-                ? " border-t-4 md:border-t-0 md:border-l-4 border-main"
-                : ""
-            }`}
-          >
-            <Link href={"/songs"}>
-              <MusicNoteOutlinedIcon />
-              <div>Songs</div>
-            </Link>
-          </li>
-          <li
-            className={`flex box py-4 md:py-2 justify-center flex-1 text-sm flex-col items-center   ${
-              currentPage === "playlists"
-                ? " border-t-4 md:border-t-0 md:border-l-4 border-main"
-                : ""
-            }`}
-          >
-            <Link href={"/playlists"}>
-              <LibraryMusicIcon />
-              <div>Playlists</div>
-            </Link>
-          </li>
+          {navbarElements.map((element: NavbarElementProp) => (
+            <li
+              key={element.value}
+              className={`flex box py-4 md:py-2 justify-center flex-1 text-sm flex-col items-center ${
+                currentPage === element.value
+                  ? " border-t-4 md:border-t-0 md:border-l-4 border-main bg-grayBg"
+                  : ""
+              }`}
+            >
+              <Link href={element.href}>
+                {element.icon}
+                <div>{element.text}</div>
+              </Link>
+            </li>
+          ))}
+          
         </ul>
       </div>
     </div>

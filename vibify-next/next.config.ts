1a165a73cb5312d098dda4bde3aaa68cba4f47db
Kevin Castro <kcapupp@princeton.edu>
2024-12-03 17:51:56 -0500

    allow images

--- vibify-next/next.config.ts
+++ vibify-next/next.config.ts
@@ -12,6 +12,17 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'i.scdn.co',
       },
+      {
+        protocol:'https',
+        hostname:'mosaic.scdn.co'
+      },
+      {
+        protocol:'https',
+        hostname:'image-cdn-ak.spotifycdn.com'
+      },{
+        protocol: 'https',
+        hostname: '**',  // This matches any hostname
+      },
     ],
   },
 };

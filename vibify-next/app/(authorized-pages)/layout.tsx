aa3ae3d78984e4dddb44d4e16ebdf64205746478
Kevin Castro <kcapupp@princeton.edu>
2024-12-04 18:09:46 -0500

    padding on mobile for main comps

--- vibify-next/app/(authorized-pages)/layout.tsx
+++ vibify-next/app/(authorized-pages)/layout.tsx
@@ -7,7 +7,7 @@ export default async function Layout({ children }: PropsWithChildren<unknown>) {
       <div className="flex md:flex-col flex-row min-w-screen">
         <Navbar />
         <main
-          className={`w-full md:pl-[100px] pb-[100px] md:pb-0 bg-grayBg min-h-screen`}
+          className={`w-full md:pl-[100px] pb-[80px] md:pb-0 bg-grayBg min-h-screen`}
         >
           <div className="md:p-10 p-6 h-full">{children}</div>
         </main>

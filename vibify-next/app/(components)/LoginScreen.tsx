c7694ef017198001477f734fb6140ccbf53947cd
Kevin Castro <kcapupp@princeton.edu>
2024-12-04 16:27:11 -0500

    RM spotify routes + access tokens file bc it was replaced with nextauth

--- vibify-next/app/(components)/LoginScreen.tsx
+++ vibify-next/app/(components)/LoginScreen.tsx
@@ -4,11 +4,9 @@ import Button from "./Buttons/Button";
 import { signIn } from "next-auth/react";
 
 const LoginScreen = () => {
-  const LOGIN_URI = "http://localhost:3000/api/spotify/login";
 
   const handleLoginClick = async () => {
-    // window.location.href = LOGIN_URI;
-    await signIn("spotify", { callbackUrl: "http://localhost:3000/dashboard" });
+    await signIn("spotify", { callbackUrl: `${process.env.NEXTAUTH_URL}/dashboard` });
 
   };
 

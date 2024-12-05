c1a2f0197017373834d82767a7347f191916aca1
Kevin Castro <kcapupp@princeton.edu>
2024-12-05 18:47:37 -0500

    rm nn divs + change styling

--- vibify-next/app/(components)/Recs/RecsComponent.tsx
+++ vibify-next/app/(components)/Recs/RecsComponent.tsx
@@ -21,6 +21,7 @@ import FeedTrackComponent from "./Feed/FeedTrackComponent";
 import FeedArtistComponent from "./Feed/FeedArtistComponent";
 import FeedAlbumComponent from "./Feed/FeedAlbumComponent";
 import FeedPlaylistComponent from "./Feed/FeedPlaylistComponent";
+import { defaultFeedType } from "@/app/types/filters";
 
 const PlayCircleOutlinedIcon = dynamic(
   () => import("@mui/icons-material/PlayCircleOutlined"),
@@ -43,6 +44,12 @@ const FavoriteBorderIcon = dynamic(
 const AddIcon = dynamic(() => import("@mui/icons-material/Add"), {
   ssr: false,
 });
+const VisibilityOutlinedIcon = dynamic(
+  () => import("@mui/icons-material/VisibilityOutlined"),
+  {
+    ssr: false,
+  }
+);
 
 interface RecsComponentProps {
   accessToken: string;
@@ -60,7 +67,7 @@ const RecsComponent = ({
 }: RecsComponentProps) => {
   const [recommendModal, setRecommenModal] = useState(false);
 
-  const [feedType, setFeedType] = useState("all");
+  const [feedType, setFeedType] = useState(defaultFeedType);
 
   const handleModalToggle = () => {
     setRecommenModal((prev) => !prev);
@@ -74,7 +81,6 @@ const RecsComponent = ({
     type: RecommendationType, // Use RecommendationType directly
     item: Artist | Track | Album | Playlist
   ) => {
-    console.log(type);
     switch (type) {
       case RecommendationType.artist:
         console.log("skjsddd");
@@ -125,83 +131,92 @@ const RecsComponent = ({
                 ))}
               </div>
             </div>
-            <div className="w-full">
-              <div className="flex flex-col gap-6">
-                {recommendations.map((item: Recommendations, index: number) => (
-                  <div className="flex flex-col gap-2 w-full" key={index}>
-                    <div className="flex gap-4 w-full">
-                      <Image
-                        className="rounded-full"
-                        src={item.users?.image_url as string}
-                        alt="User Profile Image"
-                        width={25}
-                        height={25}
-                        style={{ alignSelf: "flex-start" }} // Prevent the image from stretching
-                      />
+            <div className="w-full flex flex-col">
+              {recommendations.map((item: Recommendations, index: number) => (
+                <div
+                  className="flex flex-col gap-2 w-full py-6 border-b-[0.5px] border-b-gray-500"
+                  key={index}
+                >
+                  <div className="flex gap-4 w-full">
+                    <Image
+                      className="rounded-full"
+                      src={item.users?.image_url as string}
+                      alt="User Profile Image"
+                      width={25}
+                      height={25}
+                      style={{ alignSelf: "flex-start" }} // Prevent the image from stretching
+                    />
 
-                      <div className="flex justify-between gap-2 w-full min-w-0">
-                        <div className="flex flex-col min-w-0">
-                          <p className="text-ellipsis overflow-hidden whitespace-nowrap">
-                            {item.users?.email}
-                          </p>
-                        </div>
-                      </div>
+                    <div className="flex w-full">
+                      <p className="text-ellipsis overflow-hidden whitespace-nowrap">
+                        {item.users?.email}
+                      </p>
                     </div>
-                    <div className="flex flex-col items-center py-4 border-y-[0.5px] border-y-gray-500">
-                      {renderRecomendationItem(
-                        item.recommendation_type,
-                        item.recommended_item
-                      )}
+                  </div>
+                  <div className="flex flex-col items-center py-4 ">
+                    {renderRecomendationItem(
+                      item.recommendation_type,
+                      item.recommended_item
+                    )}
+                  </div>
+                  <div className="flex gap-2">
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      <FavoriteBorderIcon
+                        style={{
+                          width: iconButtonSize,
+                          height: iconButtonSize,
+                        }}
+                      />
+                      <span style={{ marginLeft: 4 }}>123</span>{" "}
+                      {/* Dummy number */}
                     </div>
-                    <div className="flex gap-2">
-                      <div style={{ display: "flex", alignItems: "center" }}>
-                        <FavoriteBorderIcon
-                          style={{
-                            width: iconButtonSize,
-                            height: iconButtonSize,
-                          }}
-                        />
-                        <span style={{ marginLeft: 4 }}>123</span>{" "}
-                        {/* Dummy number */}
-                      </div>
-                      <div style={{ display: "flex", alignItems: "center" }}>
-                        <ChatBubbleOutlineIcon
-                          style={{
-                            width: iconButtonSize,
-                            height: iconButtonSize,
-                          }}
-                        />
-                        <span style={{ marginLeft: 4 }}>456</span>{" "}
-                        {/* Dummy number */}
-                      </div>
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      <ChatBubbleOutlineIcon
+                        style={{
+                          width: iconButtonSize,
+                          height: iconButtonSize,
+                        }}
+                      />
+                      <span style={{ marginLeft: 4 }}>456</span>{" "}
+                      {/* Dummy number */}
+                    </div>
+                    {item.recommendation_type == "track" ? (
                       <PlayCircleOutlinedIcon
                         style={{
                           width: iconButtonSize,
                           height: iconButtonSize,
                         }}
                       />
-                      <AddIcon
+                    ) : (
+                      <VisibilityOutlinedIcon
                         style={{
                           width: iconButtonSize,
                           height: iconButtonSize,
                         }}
                       />
-                    </div>
+                    )}
 
-                    <div className="flex justify-between gap-2 w-full min-w-0">
-                      <div className="flex flex-col min-w-0">
-                        <p className="">
-                          <span className="mr-2 font-bold">
-                            {item.users?.email}
-                          </span>
-                          <span className="text-lightGray text-wrap"></span>
-                          {item.text}
-                        </p>
-                      </div>
+                    <AddIcon
+                      style={{
+                        width: iconButtonSize,
+                        height: iconButtonSize,
+                      }}
+                    />
+                  </div>
+
+                  <div className="flex justify-between gap-2 w-full">
+                    <div className="flex flex-col">
+                      <p className="">
+                        <span className="mr-2 font-bold">
+                          {item.users?.email}
+                        </span>
+                        <span className="text-lightGray text-wrap"></span>
+                        {item.text}
+                      </p>
                     </div>
                   </div>
-                ))}
-              </div>
+                </div>
+              ))}
             </div>
             <div className="fixed md:bottom-[20px] bottom-[100px] right-[20px] ">
               <CircleButton onClick={() => handleModalToggle()} size={60}>

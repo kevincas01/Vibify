d30cf2aff6b16528505bcebc412cf2c37c681172
Kevin Castro <kcapupp@princeton.edu>
2024-12-11 18:09:40 -0500

    Spotify player

--- vibify-next/app/(components)/SpotifyPlayer.tsx
+++ vibify-next/app/(components)/SpotifyPlayer.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from "react";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import PauseIcon from "@mui/icons-material/Pause";
@@ -5,99 +6,37 @@ import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
 import SkipNextIcon from "@mui/icons-material/SkipNext";
 import { Artist, Track } from "../types/spotify";
 import Image from "next/image";
-import {
-  getAvailableDevices,
-  getPlaybackState,
-  pausePlayback,
-  startResumeTrackPlayback,
-} from "../utils/spotify";
-import { getSession } from "next-auth/react";
-import { SpotifyAPI } from "../utils/clients/spotify";
 
-interface SpotifyPlayerProps {
-  track: Track;
-  list?: Track[];
-}
+import { useTrackInfo } from "../context/player";
 
-const SpotifyPlayer = ({ track }: SpotifyPlayerProps) => {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [deviceId, setDeviceId] = useState("");
+interface SpotifyPlayerProps {}
 
-  // Function to handle track play
-  const handleTrackPlay = async (uri: string, device: string) => {
-    const session = await getSession();
-    const accessToken = session?.user.accessToken as string;
-    console.log(device);
-    await startResumeTrackPlayback(accessToken, undefined, [uri], device);
-    setIsPlaying(true); // Set isPlaying to true when track is played
-  };
+const SpotifyPlayer = ({}: SpotifyPlayerProps) => {
+  // const [deviceId, setDeviceId] = useState("");
+  const { currentTrack, isPlaying, handlePlayPause } = useTrackInfo();
 
-
-  const handlePlayPause = async () => {
-    const session = await getSession();
-    const accessToken = session?.user.accessToken as string;
-
-    const playbackState = await getPlaybackState(accessToken);
-    if (!playbackState.is_playing) {
-      await startResumeTrackPlayback(
-        accessToken,
-        undefined,
-        undefined,
-        undefined
-      );
-      setIsPlaying(true)
-    } else {
-      pausePlayback(accessToken);
-      setIsPlaying(false)
-    }
-  };
-
-  // Function to fetch the playback state and handle devices
-  useEffect(() => {
-    const handleDevices = async () => {
-      const session = await getSession();
-      const accessToken = session?.user.accessToken as string;
-
-      const playbackState = await getPlaybackState(accessToken);
-      console.log(playbackState);
-
-      const device = playbackState.device.id;
-      setDeviceId(device);
-
-      if (playbackState.is_playing && playbackState.item.id !== track.id) {
-        // If playback is already happening on another track, switch to the new track
-        setIsPlaying(true);
-        handleTrackPlay(track.uri, device);
-      } else if (!playbackState.is_playing) {
-        // If nothing is playing, start the track
-        handleTrackPlay(track.uri, device);
-      }
-    };
-
-    handleDevices();
-  }, [track.uri]); // Depend on track.uri to handle new tracks
-
+  if (currentTrack === null) return null;
   return (
     <div className="w-full md:pl-[108px] bg-grayBg py-4 px-2 fixed bottom-0 left-0 m-auto md:z-10 z-30">
-      <div className="grid md:grid-cols-3 items-center grid-cols-2">
+      <div className="grid md:grid-cols-2 items-center grid-cols-2">
         <div className="flex flex-col gap-4">
-          <li key={track.id} className="flex gap-4 w-full">
+          <li key={currentTrack.id} className="flex gap-4 w-full">
             <Image
-              src={track.album.images[0].url}
-              alt={track.name}
+              src={currentTrack.album.images[0].url}
+              alt={currentTrack.name}
               width={50}
               height={50}
             />
             <div className="flex justify-between gap-2 w-full min-w-0">
               <div className="flex flex-col min-w-0">
                 <p className="text-ellipsis overflow-hidden whitespace-nowrap">
-                  {track.name}
+                  {currentTrack.name}
                 </p>
                 <p className="text-lightGray text-ellipsis overflow-hidden whitespace-nowrap">
-                  {track.artists.map((artist: Artist, index: number) => (
+                  {currentTrack.artists.map((artist: Artist, index: number) => (
                     <React.Fragment key={index}>
                       {artist.name}
-                      {index < track.artists.length - 1 && " ~ "}
+                      {index < currentTrack.artists.length - 1 && " ~ "}
                     </React.Fragment>
                   ))}
                 </p>
@@ -111,14 +50,14 @@ const SpotifyPlayer = ({ track }: SpotifyPlayerProps) => {
             <PlayArrowIcon
               fontSize="inherit"
               onClick={() => {
-                handlePlayPause(); 
+                handlePlayPause();
               }}
             />
           ) : (
             <PauseIcon
               fontSize="inherit"
               onClick={() => {
-                handlePlayPause(); 
+                handlePlayPause();
               }}
             />
           )}

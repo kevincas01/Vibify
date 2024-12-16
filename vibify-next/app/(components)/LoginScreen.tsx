cd0de89ed8e1bf5a2057b7e90fc7abd20ae65a99
Kevin Castro <kcapupp@princeton.edu>
2024-12-16 14:10:21 -0500

    callback url

--- vibify-next/app/(components)/LoginScreen.tsx
+++ vibify-next/app/(components)/LoginScreen.tsx
@@ -4,7 +4,9 @@ import Button from "./Buttons/Button";
 import { signIn } from "next-auth/react";
 
 const LoginScreen = () => {
-  const loginUrl=`${process.env.NEXT_PUBLIC_NEXTAUTH_URL}`
+  const loginUrl =process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+  process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+  'http://localhost:3000/';
 
   const handleLoginClick = async () => {
     await signIn("spotify", { callbackUrl: `${loginUrl}/dashboard` });
